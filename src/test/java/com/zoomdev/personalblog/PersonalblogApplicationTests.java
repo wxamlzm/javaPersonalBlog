434ce2bc9204009ac71f695b1d573e8efc888300
wxamlzm <zd5024@163.com>
2024-09-16 20:27:46 +0800

    INIT

---
+++ src/test/java/com/zoomdev/personalblog/PersonalblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoomdev.personalblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
