9f8ae33ab19f3f83339d71fafc7ad5aad7e6f442
wxamlzm <zd5024@163.com>
2024-10-10 09:51:38 +0800

    UPDATE：restful化接口

--- src/main/java/com/zoomdev/personalblog/controller/UserController.java
+++ src/main/java/com/zoomdev/personalblog/controller/UserController.java
@@ -1,21 +1,27 @@
 package com.zoomdev.personalblog.controller;
 
 import com.zoomdev.personalblog.Response;
+import com.zoomdev.personalblog.model.dto.CreateUserDTO;
 import com.zoomdev.personalblog.model.dto.UserDTO;
 import com.zoomdev.personalblog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     public Response<UserDTO> getUserById(@PathVariable long id) {
         return Response.newSuccess(userService.getUserById(id));
+    }
+
+    @PostMapping
+    public Response createUser(@RequestBody CreateUserDTO createUserDTO){
+        UserDTO createdUser = userService.createUser(createUserDTO);
+        return Response.newSuccess(createdUser);
     }
 }
